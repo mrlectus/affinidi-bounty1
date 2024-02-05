336f24c40ec12148fffaad680fe5a489ea2a61ae
BrownCarter <lilbrown3000@gmail.com>
2024-02-05 16:49:37 +0100

    fix session

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import { getSession } from "@auth0/nextjs-auth0";
 export default async function Home() {
-  const { user } = await getSession();
+  const session = await getSession();
+  const user = session?.user;
   console.log(user);
   return (
     <main>
-      <a href="/api/auth/login">Login {user.nickname}</a>
+      <a href="/api/auth/login">Login {user?.nickname}</a>
     </main>
   );
 }
